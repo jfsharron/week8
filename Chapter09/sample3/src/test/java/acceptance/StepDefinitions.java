4a8530d0da4027df6566b226bc36bb03e04f2681
Jonas Sharron <jfsharron@msn.com>
2021-10-22 12:25:50 -0700

    Update StepDefinitions.java
    returned stepdefinitions to origninal

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -18,7 +18,7 @@ public class StepDefinitions {
     private String result;
 
     @Given("^I have two numbers: (.*) and (.*)$")
-    public void there_are_two_numbers(String a, String b) throws Throwable {
+    public void i_have_two_numbers(String a, String b) throws Throwable {
         this.a = a;
         this.b = b;
     }
@@ -31,25 +31,6 @@ public class StepDefinitions {
 
     @Then("^I receive (.*) as a result$")
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
-        assertEquals(expectedResult, result);
-    }
-
-
-
-    @Given("^I have two numbers: (.*) and (.*)$")
-    public void i_have_two_numbers(String a, String b) throws Throwable {
-        this.a = a;
-        this.b = b;
-    }
-
-    @When("^the calculator sums them$")
-    public void the_calculator_sums_them() throws Throwable {
-        String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
-        result = restTemplate.getForObject(url, String.class);
-    }
-
-    @Then("^I receive (.*) as the quotient$")
-    public void i_receive_as_the_quotient(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
     }
 }
